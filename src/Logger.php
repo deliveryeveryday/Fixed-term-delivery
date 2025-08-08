d3cc9237069349ef68a842f20589e9c82171c47c
deliveryeveryday <mamaitao@yahoo.co.jp>
2025-08-08 09:20:22 +0900

    Add files via upload

---
+++ src/Logger.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger as MonologLogger;
+
+class Logger
+{
+    private static ?MonologLogger $instance = null;
+
+    public static function getInstance(array $config): MonologLogger
+    {
+        if (self::$instance === null && $config['enabled']) {
+            self::$instance = new MonologLogger('app');
+            self::$instance->pushHandler(new StreamHandler($config['file'], $config['level']));
+        }
+        return self::$instance;
+    }
+}
