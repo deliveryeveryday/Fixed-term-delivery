ff6cf4ff8b1e1d8e95c61486b4c461e8764d10f2
deliveryeveryday <mamaitao@yahoo.co.jp>
2025-08-09 09:25:40 +0900

    Add files via upload

--- src/Logger.php
+++ src/Logger.php
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
 <?php
 namespace App;
-
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger as MonologLogger;
-
-class Logger
-{
+class Logger {
     private static ?MonologLogger $instance = null;
-
-    public static function getInstance(array $config): MonologLogger
-    {
+    public static function getInstance(array $config): ?MonologLogger {
         if (self::$instance === null && $config['enabled']) {
             self::$instance = new MonologLogger('app');
             self::$instance->pushHandler(new StreamHandler($config['file'], $config['level']));
         }
         return self::$instance;
     }
-=======
-<?php
-namespace App;
-
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger as MonologLogger;
-
-class Logger
-{
-    private static ?MonologLogger $instance = null;
-
-    public static function getInstance(array $config): MonologLogger
-    {
-        if (self::$instance === null && $config['enabled']) {
-            self::$instance = new MonologLogger('app');
-            self::$instance->pushHandler(new StreamHandler($config['file'], $config['level']));
-        }
-        return self::$instance;
-    }
->>>>>>> e2f911496a3137af6bf699c4e596ffe0f3a3fc48
 }
